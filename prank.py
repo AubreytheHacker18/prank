d2b4d562b65fb9e90e70ff9570364a9d4b2b01fd
AubreytheHacker18 <44407174+AubreytheHacker18@users.noreply.github.com>
2019-01-20 08:22:30 +0200

    Create prank.py

---
+++ prank.py
@@ -0,0 +1,10 @@
+from tkinter import Tk, scrolledtext, Menu, filedialog, END, messagebox, simpledialog
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
+def open():
+    root = Tk()
+    root.title("Minecraft Launcher")
+    root.geometry("0x0")
+    showerror("Error", "You have no space on this pc...")
+open()
